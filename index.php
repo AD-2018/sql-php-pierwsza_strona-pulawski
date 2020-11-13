29e6402d58487ece8f6e368e9d673fe72d4db9a7
pulawski <73634660+pulawski@users.noreply.github.com>
2020-11-13 10:48:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html >
+<head>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
 <?php
-echo "Paweł Puławski";
+$servername = "mysql-pawelpulawski123.alwaysdata.net";
+$username = "218263";
+$password = "Q@wertyuiop";
+$dbname = "pawelpulawski123_jd	";
+ 
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+$sql= "SELECT * FROM pracownicy";
+echo("<h3>zadanie </h3>");
+echo("<li>".$sql."<br><br>");
+$result = mysqli_query($conn, $sql);
+echo('<table border="1" class="tabelka_moja">');
+while($row = mysqli_fetch_assoc($result) ) {
+   echo('<tr>');
+   echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+   echo('</tr>');
+}
+echo('</table>');
 ?>
+</html>
