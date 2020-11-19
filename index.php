3d004e1302944d41e749688817f1670d49698c4b
pulawski <73634660+pulawski@users.noreply.github.com>
2020-11-19 09:02:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
+ <a href="formularz.html">formularze</a>
 <?php
 $servername = "mysql-kcz.alwaysdata.net";
 $username = "kcz";
