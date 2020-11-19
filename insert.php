7673344994331611322056105bc070bdb901d807
pulawski <73634660+pulawski@users.noreply.github.com>
2020-11-19 09:19:01 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
 <?php
-echo("jestes w insert.php");
-echo $_POST['name'];
+echo("jesteś w insert.php");
+
+echo("<ul>");
+	
+echo("<li>".$_POST["imie"]);
+		
+echo("<ul>");
 ?>
-  </body>
-</html>
