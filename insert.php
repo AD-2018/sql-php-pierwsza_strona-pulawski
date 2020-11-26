c700e0a76d265065d726b12b6db293f5dffc92d0
pulawski <73634660+pulawski@users.noreply.github.com>
2020-11-26 08:55:55 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,26 @@
 <?php
-echo("jesteś w insert.php");
 
-echo("<ul>");
-	
-echo("<li>".$_POST["imie"]);
-		
-echo("<ul>");
+$servername = "mysql-kcz.alwaysdata.net";
+$username = "kcz";
+$password = "zaq1@WSX";
+$dbname = "kcz_20";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (id, imie, dzial, zarobki,data_urodzenia) 
+       VALUES (null,'Balbina', 4, 86,'1999-05-21')";
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
