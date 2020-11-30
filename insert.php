28504810c55438258df95014708e3b434c9ae25a
pulawski <73634660+pulawski@users.noreply.github.com>
2020-12-01 00:08:00 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,26 +1,28 @@
 <?php
-
 $servername = "mysql-kcz.alwaysdata.net";
 $username = "kcz";
 $password = "zaq1@WSX";
 $dbname = "kcz_20";
+
+echo("jestes w insert.php");
+echo "<li>".$_POST['imie'];
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['zarobki'];
+echo "<li>".$_POST['data_urodzenia'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (id, imie, dzial, zarobki,data_urodzenia) 
-       VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
-
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
-
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
-
 ?>
