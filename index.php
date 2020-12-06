408f5df595c1db1fd18dd2d4b4dfe5211486d266
pulawski <73634660+pulawski@users.noreply.github.com>
2020-12-07 00:35:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -36,27 +36,6 @@
       </ul>
   </aside>
   </nav>
-  <?php
-$servername = "mysql-kcz.alwaysdata.net";
-$username = "kcz";
-$password = "zaq1@WSX";
-$dbname = "kcz_20";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
- 
-$conn = new mysqli($servername, $username, $password, $dbname);
- 
-$sql= "SELECT * FROM pracownicy";
-echo("<h1>Paweł Puławski </h1>");
-echo("<li>".$sql."<br><br>");
-$result = mysqli_query($conn, $sql);
-echo('<table border="1" class="tabelka_moja">');
-while($row = mysqli_fetch_assoc($result) ) {
-   echo('<tr>');
-   echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-   echo('</tr>');
-}
-echo('</table>');
-?>
 </body>
 </html>
