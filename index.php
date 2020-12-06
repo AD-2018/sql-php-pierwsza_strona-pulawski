ea7035f684ddd366a291a6ff44b18e426f66af36
pulawski <73634660+pulawski@users.noreply.github.com>
2020-12-07 00:26:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
-<!DOCTYPE html >
+<!DOCTYPE html>
+<html>
 <head>
-<link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="styles.css">
+  <title>
+    Rozsuwane menu boczne w samym CSS 
+  </title>
 </head>
 <body>
- <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">•github</a>
- <br>
- <a href="formularz.html">formularze</a>
- <br>
- <a href="pracownicy_organizacja.php">Pracownicy i Organizacja </a>
- <br>
- <a href="danedobazy.php">dodawanie/usuwanie pracownika</a>
-<?php
+  <nav class="site-nav">
+    <button class="side-menu-trigger">Menu</button>
+    <aside class="side-menu">
+      <ul>
+        <li>
+          <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="formularz.html">
+            Formularze
+          </a>
+        </li>
+        <li>
+          <a href="pracownicy_organizacja.php">
+            Pracownicy i Organizacja
+          </a>
+        </li>
+        <li>
+          <a href="danedobazy.php">
+            Dodawanie i usuwanie pracownika
+          </a>
+        </li>
+      </ul>
+  </aside>
+  </nav>
+  <?php
 $servername = "mysql-kcz.alwaysdata.net";
 $username = "kcz";
 $password = "zaq1@WSX";
