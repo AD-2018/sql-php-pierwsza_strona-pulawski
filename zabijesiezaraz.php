ced746ba6d8c290994bd977ac994150981235dec
pulawski <73634660+pulawski@users.noreply.github.com>
2020-12-07 01:23:46 +0100

    Create zabijesiezaraz.php

---
+++ zabijesiezaraz.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="style.css">
+  <title>
+    Strona główna
+  </title>
+</head>
+<body>
+  <nav class="site-nav">
+    <button class="side-menu-trigger">Menu</button>
+    <aside class="side-menu">
+      <ul>
+        <li>
+          <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="formularz.html">
+            Formularze
+          </a>
+        </li>
+        <li>
+          <a href="pracownicy_organizacja.php">
+            Pracownicy i Organizacja
+          </a>
+        </li>
+        <li>
+          <a href="danedobazy.php">
+            Dodawanie i usuwanie pracownika
+          </a>
+        </li>
+      </ul>
+  </aside>
+  </nav>
+  <h1>Paweł Puławski <h1>
+  
+</html>
