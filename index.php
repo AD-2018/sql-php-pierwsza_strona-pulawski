1ebf651a1e8e153dd8d296e7d160963c1d1a8ad5
pulawski <73634660+pulawski@users.noreply.github.com>
2020-12-07 02:12:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
     <aside class="side-menu">
       <ul>
         <li>
+          <a href="index.php">
+            Strona główna
+          </a>
+        </li
+        <li>
           <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">
             Github
           </a>
