19cff72d19f7429b52644cfa031f0966691daeaa
pulawski <73634660+pulawski@users.noreply.github.com>
2021-02-01 08:38:06 +0100

    Update danedobazy.php

--- pracownicy/danedobazy.php
+++ pracownicy/danedobazy.php
@@ -2,16 +2,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../style.css">
   <title>
-    Strona główna
+    Paweł Puławski
   </title>
 </head>
 <body>
   <nav class="site-nav">
     <button class="side-menu-trigger">Menu</button>
     <aside class="side-menu">
-     <ul>
+       <ul>
         <li>
           <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">
             Github
@@ -23,25 +23,29 @@
           </a>
         </li>
         <li>
-          <a href="pracownicy_organizacja.php">
+          <a href="pracownicy/pracownicy_organizacja.php">
             Pracownicy organizacja
           </a>
         </li>
         <li>
-          <a href="funkcje_agregujace.php">
+          <a href="pracownicyfunkcje_agregujace.php">
             Funkcje Agregujące
           </a>
         </li>
         <li>
-          <a href="danedobazy.php">
+          <a href="pracownicy/danedobazy.php">
             Dodawanie i usuwanie pracownika
           </a>
         </li>
         <li>
-          <a href="formularz.html">
+          <a href="inne/formularz.html">
             Formularz 
           </a>
         </li>
+        <li>
+          <a href="inne/dataczas.php">
+            Data i czas 
+          </a>
       </ul>
   </aside>
   </nav>
