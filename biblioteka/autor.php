79c371e800d86ecb13c354c32f0f6b51726d0bae
pulawski <73634660+pulawski@users.noreply.github.com>
2021-02-04 14:01:22 +0100

    Update autor.php

--- biblioteka/autor.php
+++ biblioteka/autor.php
@@ -1,1 +1,27 @@
+<?php
+echo("jestes w autor.php <br>");
+echo $_POST['autor'];
 
+$servername = "mysql-kcz.alwaysdata.net";
+$username = "kcz";
+$password = "zaq1@WSX";
+$dbname = "kcz_20";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_autor(id_autor, autor) VALUES(null, "$_POST['autor']");
+echo $sql;
+if ($conn->query($sql) === TRUE) {
+  echo "dodano";
+  header('Location: https://pulawski-php.herokuapp.com/biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
