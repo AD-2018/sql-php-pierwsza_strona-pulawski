6ad6a69570516cbf061be441be692172672298b0
pulawski <pulawski.pawel11@gmail.com>
2021-04-18 21:04:10 +0200

    pyk

--- pracownicy/danedobazy.php
+++ pracownicy/danedobazy.php
@@ -8,58 +8,7 @@
   </title>
 </head>
 <body>
-  <nav class="site-nav">
-    <button class="side-menu-trigger">Menu</button>
-    <aside class="side-menu">
-    <ul>
-        <li>
-          <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-pulawski">
-            Github
-          </a>
-        </li>
-        <li>
-          <a href="../index.php">
-            Strona główna
-          </a>
-        </li>
-        <li>
-          <a href="../pracownicy/pracownicy_organizacja.php">
-            Pracownicy organizacja
-          </a>
-        </li>
-        <li>
-          <a href="../pracownicy/funkcje_agregujace.php">
-            Funkcje Agregujące
-          </a>
-        </li>
-        <li>
-          <a href="../pracownicy/danedobazy.php">
-            Dodawanie i usuwanie pracownika
-          </a>
-        </li>
-        <li>
-          <a href="../inne/formularz.html">
-            Formularz 
-          </a>
-        </li>
-        <li>
-          <a href="../inne/dataczas.php">
-            Data i czas 
-          </a>
-        </li>
-        <li>
-          <a href="../biblioteka/biblioteka.php">
-            Biblioteka
-          </a>
-        </li>
-        <li>
-            <a href="../grid/indexgrid.html">
-              Grid
-            </a>
-          </li>
-      </ul>
-  </aside>
-  </nav>
+<?php  include "../menu.php"; ?>
   <h1>Paweł Puławski <h1>
  
 
