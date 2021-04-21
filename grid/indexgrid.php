4345ae10f3e91231a2f514f72017d65730e7a398
pulawski <pulawski.pawel11@gmail.com>
2021-04-21 23:51:01 +0200

    flflflflfl

--- grid/indexgrid.php
+++ grid/indexgrid.php
@@ -12,15 +12,15 @@
   <h1>Paweł Puławski <h1>
     <div class="menu ">
         <br />
-        <a href="../grid/indexgrid.html">Główna</a><br /><br /><br />
+        <a href="../grid/indexgrid.php">Główna</a><br /><br /><br />
         <a href="strona1.php">Strona1</a><br /><br /><br />
         <a href="strona2.php">Strona2</a><br /><br /><br />
         <a href="strona3.php">Strona3</a><br /><br /><br />
         <a href="strona4.php">Strona4</a><br /><br /><br />
-        <a href="strona5.html">Strona5</a><br /><br /><br />
-        <a href="strona6.html">Strona6</a><br /><br /><br />
-        <a href="strona7.html">Strona7</a><br /><br /><br />
-        <a href="strona8.html">Strona8</a><br /><br /><br />
+        <a href="strona5.php">Strona5</a><br /><br /><br />
+        <a href="strona6.php">Strona6</a><br /><br /><br />
+        <a href="strona7.php">Strona7</a><br /><br /><br />
+        <a href="strona8.php">Strona8</a><br /><br /><br />
         <a href="strona9.html">Strona9</a><br /><br /><br />
         <a href="strona10.html">Strona10</a><br /><br /><br />
         <a href="strona11.html">Strona11</a><br /><br /><br />
